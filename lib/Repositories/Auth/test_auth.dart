92e8ffcf29c65afb0f9e6df29a52fdf4b875e73f
Dylan Baker <dylanbaker7@gmail.com>
2021-10-22 18:43:50 -0400

    fixed auth stream'

--- lib/Repositories/Auth/test_auth.dart
+++ lib/Repositories/Auth/test_auth.dart
@@ -1,17 +1,22 @@
+import 'dart:async';
+
 import 'package:stripe_package/Repositories/Auth/auth.dart';
 
 class TestAuth extends AuthRepo {
   AuthUser _user = AuthUser.empty();
 
+  StreamController<AuthUser> _authUser = StreamController();
   @override
   Future<void> logIn(Map<String, dynamic> params) async {
     print('Logged in');
     _user = const AuthUser(uid: 'uid');
+    _authUser.add(_user);
   }
 
   @override
   Future<void> logOut() async {
     _user = AuthUser.empty();
+    _authUser.add(_user);
   }
 
   @override
@@ -23,8 +28,9 @@ class TestAuth extends AuthRepo {
   Future<void> signUp(Map<String, dynamic> params) async {
     print('Signed up');
     _user = const AuthUser(uid: 'uid');
+    _authUser.add(_user);
   }
 
   @override
-  Stream<AuthUser> get user => Stream.value(_user);
+  Stream<AuthUser> get user => _authUser.stream;
 }

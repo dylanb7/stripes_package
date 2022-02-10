bbf8fdb57bfacbd07ed11fae012281890ed882b7
Dylan Baker <dylanbaker7@gmail.com>
2022-02-10 13:56:11 -0500

    changed stamp bloc to emit on edit

--- lib/Blocs/Stamps/bloc/stamp_bloc.dart
+++ lib/Blocs/Stamps/bloc/stamp_bloc.dart
@@ -8,8 +8,12 @@ part 'stamp_event.dart';
 part 'stamp_state.dart';
 part 'stamp_bloc.freezed.dart';
 
+const stampKey = 'stamps';
+
 class StampBloc<T extends Stamp> extends Bloc<StampEvent<T>, StampState<T>> {
   final StampRepo _repo;
+
+  Map<String, Object> stampCache = {};
 
   StreamSubscription? stampSub;
 
@@ -18,11 +22,15 @@ class StampBloc<T extends Stamp> extends Bloc<StampEvent<T>, StampState<T>> {
         super(const StampState.initial()) {
     on<StampEvent<T>>((event, emit) {
       event.map(add: (add) {
-        _repo.addStamp(add.stamp);
+        _refresh(emit, () {
+          _repo.addStamp(add.stamp);
+        });
       }, remove: (remove) {
-        _repo.removeStamp(remove.stamp);
+        _refresh(emit, () {
+          _repo.removeStamp(remove.stamp);
+        });
       }, update: (update) {
-        _repo.updateStamp(update.stamp);
+        _refresh(emit, _repo.updateStamp(update.stamp));
       }, load: (load) {
         stampSub?.cancel();
         stampSub = _repo.stamps.listen((stamps) {
@@ -30,8 +38,15 @@ class StampBloc<T extends Stamp> extends Bloc<StampEvent<T>, StampState<T>> {
         });
         emit(const StampState.loading());
       }, loaded: (loaded) {
+        stampCache[stampKey] = loaded.stamps as List<T>;
         emit(StampState.updated(loaded.stamps as List<T>));
       });
     });
+  }
+
+  _refresh(Emitter<StampState<T>> emit, Function operation) {
+    emit(const StampState.initial());
+    operation();
+    emit(StampState.updated(stampCache[stampKey] as List<T>));
   }
 }

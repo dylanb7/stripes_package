258071f3280e694e24651c9050b14b325bcee548
Dylan Baker <dylanbaker7@gmail.com>
2022-06-11 16:34:21 -0400

    added subuser as param for stamp repo

--- lib/Repositories/Stamps/stamp_repo.dart
+++ lib/Repositories/Stamps/stamp_repo.dart
@@ -5,8 +5,8 @@ part 'stamp.dart';
 
 abstract class StampRepo<T extends Stamp> {
   final AuthRepo authRepo;
-  final SubUserRepo subRepo;
-  StampRepo({required this.authRepo, required this.subRepo});
+  final SubUser currentUser;
+  StampRepo({required this.authRepo, required this.currentUser});
   Stream<List<T>> get stamps;
   addStamp(T stamp);
   removeStamp(T stamp);

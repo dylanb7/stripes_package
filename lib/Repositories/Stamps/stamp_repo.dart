ef34ee9b755df2f118255c82aaa8c108595e2740
Dylan Baker <dylanbaker7@gmail.com>
2022-06-10 20:23:41 -0400

    added required params to sub and stamp repos

--- lib/Repositories/Stamps/stamp_repo.dart
+++ lib/Repositories/Stamps/stamp_repo.dart
@@ -1,6 +1,12 @@
+import 'package:stripe_package/Repositories/Auth/auth.dart';
+import 'package:stripe_package/Repositories/SubUser/sub_user_repo.dart';
+
 part 'stamp.dart';
 
 abstract class StampRepo<T extends Stamp> {
+  final AuthRepo authRepo;
+  final SubUser subUser;
+  StampRepo({required this.authRepo, required this.subUser});
   Stream<List<T>> get stamps;
   addStamp(T stamp);
   removeStamp(T stamp);
--- lib/Repositories/SubUser/sub_user_repo.dart
+++ lib/Repositories/SubUser/sub_user_repo.dart
@@ -2,11 +2,14 @@ import 'dart:core';
 
 import 'package:flutter/material.dart';
 import 'package:stripe_package/Constants/db_keys.dart';
+import 'package:stripe_package/Repositories/Auth/auth.dart';
 import 'package:uuid/uuid.dart';
 
 part 'sub_user.dart';
 
 abstract class SubUserRepo {
+  final AuthRepo authRepo;
+  SubUserRepo({required this.authRepo});
   Stream<List<SubUser>> get users;
   SubUser? get current;
   void updateCurrent(SubUser current);
